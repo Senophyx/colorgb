5d1cdf2ac407638637bac57b8f9d79cebb781ac3
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-26 16:44:28 +0700

    Create `__init__`

---
+++ recolor/__init__.py
@@ -0,0 +1,16 @@
+__title__ = "recolor"
+__version__ = "1.0"
+__authors__ = "LyQuid"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2022-present LyQuid"
+
+try:
+    from colorama import init
+except ImportError:
+    raise TypeError("You need install colorama modules. Install : pip install colorama")
+
+init()
+
+from .foregrounds import fore
+from .backrounds import bg
+from .styles import style
