f23fc0fd2246476ad50131cfd3244dd37db4abc6
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-27 14:06:31 +0700

    Added lot of style

--- colorgb/styles.py
+++ colorgb/styles.py
@@ -1,6 +1,10 @@
 class styles:
-    UNDERLINE = "\u001b[4m"
     BOLD = "\u001b[1m"
+    ITALIC = "\u001b[3m"
+    UNDERLINE = "\u001b[4m"
+    REVERSE = "\u001b[7m"
+    HIDDEN = "\u001b[8m"
+    OVERLINED = "\u001b[53m"
     RESET = "\033[0m"
 
 def style(text=None, style=None, reset=True) -> str:
@@ -9,20 +13,48 @@ def style(text=None, style=None, reset=True) -> str:
     -
     -----
     List :
-    - `underline`
     - `bold`
+    - `italic` (Not widely supported)
+    - `underline`
+    - `reverse` (Swap foreground and background colors)
+    - `hidden`
+    - `overlined` (Not supported in Terminal app)
     """
-    if style == "underline":
-        if reset == False:
-            styled = f"{styles.UNDERLINE}{text}"
-        else:
-            styled = f"{styles.UNDERLINE}{text}{styles.RESET}"
-        
-    elif style == "bold":
+    if style == "bold":
         if reset == False:
             styled = f"{styles.BOLD}{text}"
         else:
             styled = f"{styles.BOLD}{text}{styles.RESET}"
+
+    elif style == "italic":
+        if reset == False:
+            styled = f"{styles.ITALIC}{text}"
+        else:
+            styled = f"{styles.ITALIC}{text}{styles.RESET}"
+
+    elif style == "underline":
+        if reset == False:
+            styled = f"{styles.UNDERLINE}{text}"
+        else:
+            styled = f"{styles.UNDERLINE}{text}{styles.RESET}"
+
+    elif style == "reverse":
+        if reset == False:
+            styled = f"{styles.REVERSE}{text}"
+        else:
+            styled = f"{styles.REVERSE}{text}{styles.RESET}"
+
+    elif style == "hidden":
+        if reset == False:
+            styled = f"{styles.HIDDEN}{text}"
+        else:
+            styled = f"{styles.HIDDEN}{text}{styles.RESET}"
+
+    elif style == "overlined":
+        if reset == False:
+            styled = f"{styles.OVERLINED}{text}"
+        else:
+            styled = f"{styles.OVERLINED}{text}{styles.RESET}"
 
     else:
         raise TypeError("Unknow style!")
