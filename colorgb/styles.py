fa474c917b86ea68d2a499ebbedbae258de8537b
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-27 13:10:33 +0700

    Added `underline` and `bold` to style

--- colorgb/styles.py
+++ colorgb/styles.py
@@ -1,4 +1,30 @@
-from .foregrounds import fore
+class styles:
+    UNDERLINE = "\u001b[4m"
+    BOLD = "\u001b[1m"
+    RESET = "033[0m"
 
 def style(text=None, style=None, reset=True) -> str:
-    return fore("SOON!", "red")
+    """
+    Style. Styling the font.
+    -
+    -----
+    List :
+    - `underline`
+    - `bold`
+    """
+    if style == "underline":
+        if reset == False:
+            styled = f"{styles.UNDERLINE}{text}"
+        else:
+            styled = f"{styles.UNDERLINE}{text}{styles.RESET}"
+        
+    if style == "bold":
+        if reset == False:
+            styled = f"{styles.BOLD}{text}"
+        else:
+            styled = f"{styles.BOLD}{text}{styles.RESET}"
+
+    else:
+        raise TypeError("Unknow style!")
+    
+    return styled
