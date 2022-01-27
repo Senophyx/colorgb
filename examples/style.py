b41e810458ee431ab0600a52a88f93e7d7db9ed6
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-27 14:24:42 +0700

    Create style.py

---
+++ examples/style.py
@@ -0,0 +1,8 @@
+import colorgb
+
+print(colorgb.style("Bold", "bold"))
+print(colorgb.style("Italic", "italic"))
+print(colorgb.style("Underline", "underline"))
+print(colorgb.style("Rerverse", "reverse"))
+print(colorgb.style("Hidden", "hidden"))
+print(colorgb.style("Overlined", "overlined"))
