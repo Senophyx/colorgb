a42839c3b5bbaa9bc40b93b3367bdcfedb194785
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2022-07-29 21:00:58 +0700

    Simplify the whole code

--- colorgb/styles.py
+++ colorgb/styles.py
@@ -1,11 +1,5 @@
-class styles:
-    BOLD = "\u001b[1m"
-    ITALIC = "\u001b[3m"
-    UNDERLINE = "\u001b[4m"
-    REVERSE = "\u001b[7m"
-    HIDDEN = "\u001b[8m"
-    OVERLINED = "\u001b[53m"
-    RESET = "\033[0m"
+from .dictionary import styles
+from .errors import *
 
 def style(text:str, style:str, reset=True):
     """
@@ -25,43 +19,11 @@ def style(text:str, style:str, reset=True):
     - `hidden`
     - `overlined` (Not supported in Terminal app)
     """
-    if style == "bold":
-        if reset == False:
-            styled = f"{styles.BOLD}{text}"
-        else:
-            styled = f"{styles.BOLD}{text}{styles.RESET}"
-
-    elif style == "italic":
-        if reset == False:
-            styled = f"{styles.ITALIC}{text}"
-        else:
-            styled = f"{styles.ITALIC}{text}{styles.RESET}"
-
-    elif style == "underline":
-        if reset == False:
-            styled = f"{styles.UNDERLINE}{text}"
-        else:
-            styled = f"{styles.UNDERLINE}{text}{styles.RESET}"
-
-    elif style == "reverse":
-        if reset == False:
-            styled = f"{styles.REVERSE}{text}"
-        else:
-            styled = f"{styles.REVERSE}{text}{styles.RESET}"
-
-    elif style == "hidden":
-        if reset == False:
-            styled = f"{styles.HIDDEN}{text}"
-        else:
-            styled = f"{styles.HIDDEN}{text}{styles.RESET}"
-
-    elif style == "overlined":
-        if reset == False:
-            styled = f"{styles.OVERLINED}{text}"
-        else:
-            styled = f"{styles.OVERLINED}{text}{styles.RESET}"
-
-    else:
-        raise TypeError("Unknow style!")
     
-    return styled
+    try:
+        if reset == False:
+            return str(styles[style]+text)
+        else:
+            return str(styles[style]+text+styles['reset'])
+    except KeyError as error:
+        raise UnknownColor(error)
