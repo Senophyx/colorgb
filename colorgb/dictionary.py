866bc3ddc56e8d91abed63f280b089d28ebe314f
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2022-07-29 20:56:28 +0700

    Added styles dictionary

---
+++ colorgb/dictionary.py
@@ -0,0 +1,51 @@
+fg = {
+    "reset": "\033[0m",
+    # Normal Color
+    "black": "\033[30m",
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "purple": "\033[35m",
+    "cyan": "\033[36m",
+    "white": "\033[37m",
+    # Light Color
+    "grey": "\033[90m",
+    "lred": "\033[91m",
+    "lgreen": "\033[92m",
+    "lyellow": "\033[93m",
+    "lblue": "\033[94m",
+    "lpurple": "\033[95m",
+    "lcyan": "\033[96m"
+}
+
+background = {
+    "reset": '\033[0m',
+    # Normal Color
+    "black": '\033[40m',
+    "red": '\033[41m',
+    "green": '\033[42m',
+    "yellow": '\033[43m',
+    "blue": '\033[44m',
+    "purple": '\033[45m',
+    "cyan": '\033[46m',
+    "white": '\033[47m',
+    # Light Color
+    "grey": '\033[100m',
+    "lred": '\033[101m',
+    "lgreen": '\033[102m',
+    "lyellow": '\033[103m',
+    "lblue": '\033[104m',
+    "lpurple": '\033[105m',
+    "lcyan": '\033[106m'
+}
+
+styles = {
+    "reset": "\033[0m",
+    "bold": "\u001b[1m",
+    "italic": "\u001b[3m",
+    "underline": "\u001b[4m",
+    "reverse": "\u001b[7m",
+    "hidden": "\u001b[8m",
+    "overlined": "\u001b[53m"
+}
